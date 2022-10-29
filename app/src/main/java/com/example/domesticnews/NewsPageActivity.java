6633c0a3d580d7c45ba9ad5019dd1c1f5621817e
Pharmony <Hisilicon@163.com>
2022-10-30 00:58:30 +0800

    Add blank new page with manifest of parent

---
+++ app/src/main/java/com/example/domesticnews/NewsPageActivity.java
@@ -0,0 +1,14 @@
+package com.example.domesticnews;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NewsPageActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_page);
+    }
+}
